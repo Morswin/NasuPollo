a84df8d0d466493ba7205e08f1cb6f85e08631cb
Morswin <41349347+Morswin@users.noreply.github.com>
2025-05-06 20:45:27 +0200

    Add life point system and do some refactoring

--- nasupollo/player.gd
+++ nasupollo/player.gd
@@ -2,8 +2,6 @@ class_name Player
 extends CharacterBody3D
 
 
-signal gain_score(score: int)
-
 const SPEED = 450.0
 const SPEED_MAX = 4.5
 const SPEED_AIR = 100.0
@@ -17,19 +15,16 @@ func _physics_process(delta):
 	var input_direction = Input.get_axis("MoveLeft", "MoveRight")
 	if input_direction != 0.0:
 		if is_on_floor():
-			velocity.x += input_direction * SPEED * delta
+			velocity.x += input_direction * self.SPEED * delta
 		else:
-			velocity.x += input_direction * SPEED_AIR * delta
-		velocity.x = clamp(velocity.x, -SPEED_MAX, SPEED_MAX)
+			velocity.x += input_direction * self.SPEED_AIR * delta
+		velocity.x = clamp(velocity.x, -self.SPEED_MAX, self.SPEED_MAX)
 	else:
 		if is_on_floor():
-			velocity.x = lerp(velocity.x, 0.0, FRICTION)
+			velocity.x = lerp(velocity.x, 0.0, self.FRICTION)
 		else:
-			velocity.x = lerp(velocity.x, 0.0, FRICTION_AIR)
+			velocity.x = lerp(velocity.x, 0.0, self.FRICTION_AIR)
 	if is_on_floor() and Input.is_action_just_pressed("Jump"):
-		velocity.y += JUMP
-	velocity.y -= GRAVITY * delta
+		velocity.y += self.JUMP
+	velocity.y -= self.GRAVITY * delta
 	move_and_slide()
-
-func process_score(score: int):
-	gain_score.emit(score)
